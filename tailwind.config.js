725314a81ff6665df8571399f4d7a690932f0f87
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-09-17 12:22:16 +0400

    configure talwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'grey': '#6b7280',
+      },
+      height: {
+				"10v": "10vh",
+				"20v": "20vh",
+				"30v": "30vh",
+				"40v": "40vh",
+				"50v": "50vh",
+				"60v": "60vh",
+				"70v": "70vh",
+				"80v": "80vh",
+				"90v": "90vh",
+				"100v": "100vh",
+			},
+    },
+  },
+  plugins: [],
+}
+
