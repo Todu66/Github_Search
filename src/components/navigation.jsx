d50563dff8e1704258b09fb320d15dd9e4061d2f
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-09-18 22:29:38 +0400

    on the way to make dark mode active

---
+++ src/components/navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function navigation() {
+  return (
+    <div className="flex justify-between  ml-auto mr-auto w-11/12 ">
+      <p>devfinder</p>
+
+      <button>
+        DARK
+      </button>
+    </div>
+  );
+}
+
+export default navigation;
