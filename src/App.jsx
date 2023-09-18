d015c2a4f36e34098473c994e27b9d90aa45c3cb
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-09-18 22:27:09 +0400

    adding styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import "./index.css"
 import ApiResult from "./components/api_result"
+import Navigation from "./components/navigation"
+// #F6F8FF
 function App() {
 
   return (
-    <div className="flex justify-cente flex-col  items-center " >
-      
+    <div className="flex flex-col  items-center  h-screen gap-2 justify-start w-11/12 my-0 mx-auto">
+      <Navigation />
       <ApiResult />
-    
+      
     
     </div>
   )
