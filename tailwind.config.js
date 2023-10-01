4cf66ccb033df2abe318de9e940f5f11b332a4a6
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-01 18:11:15 +0400

    vercel fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        'grey': '#6b7280',
+      backgroundColor: {
+        'grey': "#F6F8FF",
       },
       height: {
 				"10v": "10vh",
@@ -18,8 +18,25 @@ export default {
 				"90v": "90vh",
 				"100v": "100vh",
 			},
+      maxWidth: {
+        'custom': '800px', // Define your custom maxWidth value here
+      },
+    },
+    screens: {
+      'md': '410px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+  darkMode: "class"
 }
 
