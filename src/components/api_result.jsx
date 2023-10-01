a04ee5aad50ff83c1e845e22d85313fb867fa3fe
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-01 17:56:51 +0400

    implementing darkmode and adding styles

--- src/components/api_result.jsx
+++ src/components/api_result.jsx
@@ -3,9 +3,11 @@ import "../../src/index.css";
 import Location from "../assets/location.png";
 import Twitter from "../assets/twitter.png";
 import Url from "../assets/url.png";
+import Build from '../assets/build.png'
 import axios from "axios";
 
-function apiResult() {
+
+function apiResult({ darkMode }) {
   const [username, setUsername] = useState("");
   const [userData, setUserData] = useState(null);
   const [notFound, setNotFound] = useState(false);
@@ -50,25 +52,25 @@ function apiResult() {
   }, []);
 
   return (
-    <div className="flex flex-col w-full">
+    <div className={`max-w-[768px] mx-auto  flex flex-col w-full`} >
       <div className="flex gap-5 relative">
         <input
           type="text"
           placeholder="Search GitHub username..."
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          className="p-4 px-2 bg-[#F6F8FF] rounded-xl text-left w-96"
+          className={`p-4 px-2 bg-[#F6F8FF] rounded-xl text-left text-base w-[100%]  font-normal outline-none sm:w-[600px]  md: py-3 ${darkMode ? "bg-slate-700" : "bg-blue-gray-800" }`}
         />
         <button
-          onClick={fetchData}
-          className="bg-[#0079FF] rounded-xl text-center text-white absolute ml-auto top-2 left-0 right-4 w-1/5 py-2"
+          onClick={fetchData}className="bg-[#0079FF] rounded-xl text-center text-white ml-auto top-1 left-1 right-4 w-1/5 py-2 max-custom absolute md:static custom-max-width "
+          
         >
           Search
         </button>
       </div>
 
       {/* Display user information */}
-      <div className="mt-6">
+      <div className={`mt-6`}>
         {(username || octocatData) && (
           <div className="flex flex-row justify-around">
             <img
@@ -77,9 +79,9 @@ function apiResult() {
               alt="Profile"
             />
             <div>
-              <h3 className="text-black font-semibold">{(userData || octocatData)?.login || "Not Available"}</h3>
-              <p className="text-sky-500">@{(userData || octocatData)?.login || "Not Available"}</p>
-              <p className="text-neutral-500">
+              <h3 className={`text-black font-semibold  ${darkMode ? "text-white" : "bg-grey-100"}`}>{(userData || octocatData)?.login || "Not Available"}</h3>
+              <p className="text-sky-500 font-normal">@{(userData || octocatData)?.login || "Not Available"}</p>
+              <p className="text-neutral-500 font-normal">
                 Joined {new Date((userData || octocatData)?.created_at).toLocaleDateString()}
               </p>
             </div>
@@ -87,23 +89,23 @@ function apiResult() {
         )}
         {/* Additional user information */}
         {(userData || octocatData) && (
-          <p className="text-neutral-500">{(userData || octocatData)?.bio || "Not Available"}</p>
+          <p className="text-neutral-500 my-3 font-normal">{(userData || octocatData)?.bio || "Not Available"}</p>
         )}
-        <div className="flex justify-center gap-5 rounded-lg p-3 bg-slate-100">
+        <div className={` mx-auto flex justify-center gap-5 rounded-lg p-3 ${darkMode ? "bg-gray-700 text-white" : "bg-slate-100 text-black"}`}>
           <div className="flex flex-col">
-            <p>Repos</p>
+            <p className="font-normal">Repos</p>
             <p>{(userData || octocatData)?.public_repos || "Not Available"}</p>
           </div>
           <div className="flex flex-col">
-            <p>Followers</p>
+            <p className="font-normal">Followers</p>
             <p>{(userData || octocatData)?.followers || "Not Available"}</p>
           </div>
           <div className="flex flex-col">
-            <p>Following</p>
+            <p className="font-normal">Following</p>
             <p>{(userData || octocatData)?.following || "Not Available"}</p>
           </div>
         </div>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 mt-5 font-normal">
           <div className="flex gap-3">
             <img src={Location} alt="Location" />
             <p>{(userData || octocatData)?.location || "Not Available"}</p>
@@ -115,6 +117,10 @@ function apiResult() {
           <div className="flex gap-3">
             <img src={Twitter} alt="Twitter" />
             <p>{(userData || octocatData)?.twitter_username || "Not Available"}</p>
+          </div>
+          <div className="flex gap-3">
+            <img src={Build} alt="build"/>
+            <p>{(userData || octocatData)?.html_url || "Not Available"}</p>
           </div>
         </div>
       </div>
